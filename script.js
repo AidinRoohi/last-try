13317e07a5a6984a7cb68d8c6f8c01d5cf0f9494
AidinRoohi <adn.roh@gmail.com>
2024-08-16 23:05:59 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function myFunction() {
+    document.getElementById("demo.").style.backgroundColor = "black";
+    document.getElementById("container").style.display = "block";
+}
+
+// function yourFunction() {
+//     document.getElementById("container").style.display = "block";
+// }
+
+// x = 5;
+// y = 6;
+// z = x + y;
+// document.getElementById("demo").innerHTML = "value is :" + z ;
+
+
+// const price1 = 5;
+
+// const price2 = 6;
+
+// let total = price1 + price2;
+
+// document.getElementById("demo").innerHTML =
+// "The total is: " + total;
+
+
+let  x = 10;
+// Here x is 10
+
+{  
+let x = 2;
+// Here x is 2
+}
+
+// Here x is 2
+document.getElementById("demo").innerHTML = x;
